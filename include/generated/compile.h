bcd1d4ed8dea216359b2dc230bbf48f2bf01c926
gandalf <gandalf@gandalf-desktop.(none)>
2013-05-26 23:02:51 +0300

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 29 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#29 PREEMPT Thu May 23 21:25:24 EEST 2013"
+#define LINUX_COMPILE_BY "gandalf"
+#define LINUX_COMPILE_HOST "gandalf-desktop"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
